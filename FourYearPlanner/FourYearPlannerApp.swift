66803caba381c0eba847dc3cddd07fd9a657c7f4
dondi <dondi@lmu.edu>
2024-03-23 11:32:36 -0700

    Initial Commit

---
+++ FourYearPlanner/FourYearPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourYearPlannerApp.swift
+//  FourYearPlanner
+//
+//  Created by Dondi on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FourYearPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
